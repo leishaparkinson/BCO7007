fc20de32eced19402732e527f4145ff382d800db
Leisha Parkinson <leisha.parkinson@live.vu.edu.au>
2021-11-04 22:36:06 +1100

    Update data and combine

--- twitter_mining_disney_join.R
+++ twitter_mining_disney_join.R
@@ -15,15 +15,15 @@ disney_data<-disney_data%>%flatten()
 #change the name of the file
 #if you don't you are going to lose your data!!!!
 
-disney_data%>%write_csv("27_10_2021.csv")
+disney_data%>%write_csv("Disney_04_11_2021.csv")
 
 #-------------------
 #do it when you start completing your assessment
 #merging and opening all csv files
 files<-list.files(pattern="\\.csv$",full.names = TRUE) #read files names with csv
-all_data_disney<-map_df(files, ~read_csv(.x)) #open and merge
+all_data_disney_04112021<-map_df(files, ~read_csv(.x)) #open and merge
 
 #you may have duplicate entries
-final_disney_data<-all_data_disney%>%distinct()
+final_disney_data_04112021<-all_data_disney_04112021%>%distinct()
 
-final_disney_data%>%write_csv("final_disney_data.csv")
+final_disney_data_04112021%>%write_csv("final_disney_data_04112021.csv")
